7ab8bc9931233eeaebb9cf6c0457c8dda053d9b1
Kamitotomi <rockyaaokage@gmail.com>
2020-08-17 13:11:34 +0900

    Minitest

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -5,42 +5,65 @@
 
 #問１　n以上、ｍ以下、n以上、m未満を表現する
 
-def liquid?(temprature)
+#def liquid?(temprature)
 	#引数が０以上１００未満を現した
-	0 << temprature && temprature < 100
+#	0 << temprature && temprature < 100
 	
-end
+#end
 
-puts liquid?(-1)
-puts liquid?(0)
-puts liquid?(100)
-puts liquid?(99)
+#puts liquid?(-1)
+#puts liquid?(0)
+#puts liquid?(100)
+#puts liquid?(99)
 
 #問１（別）case文を使う
 
-def charge(age)
+#def charge(age)
 
-	case age
+	#case age
 		#0歳から5歳はタダ
-	when 0..5
+#	when 0..5
 
-		0
+#		0
 
 		#6歳から12歳は300円
 
-	when 6..12
+#	when 6..12
 
-		300
+#		300
 		#大人1000円
-	else
+#	else
 
-		1000
+#		1000
 		
-	end
+#	end
+	
+#end
+
+#puts charge(3)
+#puts charge(6)
+#puts charge(12)
+#puts charge(22)
+
+
+#範囲オブジェクトにto_aを渡すと連続する配列を作成できる
+
+(1..5).to_a 
+(1...5).to_a
+
+#そんな使いかもいけんのねー
+
+('bad'..'bag').to_a
+('bad'...'bag').to_a
+
+#配列作成その２
+
+[*1..5]
+[*1...5]
+
+#テストコード
+
+def to_hex(r, g, b)
+	'#00000'
 	
 end
-
-puts charge(3)
-puts charge(6)
-puts charge(12)
-puts charge(22)
