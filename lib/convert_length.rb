086070b488d401c4338c10f111f8f4b85e280200
Kamitotomi <rockyaaokage@gmail.com>
2020-08-28 13:27:50 +0900

    Length

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,5 +1,11 @@
+#デフォルト値はなくてもいいが、ここではメートルを受け取っている
+
+UNITS = { m: 1.0, ft: 3.28, in: 39.37 }
 def convert_length(length, from: :m, to: :m)
-	units = { m: 1.0, ft: 3.28, in: 39.37 }
-	(length / units[from] * units[to]).round(2)
+	
+	(length / UNITS[from] * UNITS[to]).round(2)
 end
+
+puts convert_length(1, from: :m, to: :in)
+puts convert_length(15, from: :in, to: :m)
 
