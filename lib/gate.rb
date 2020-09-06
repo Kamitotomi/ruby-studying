a9035036fcfe88f8211502215fa55250513dfd80
Kamitotomi <rockyaaokage@gmail.com>
2020-09-06 14:14:11 +0900

    what

---
+++ lib/gate.rb
@@ -0,0 +1,37 @@
+
+# 改札機プログラムのClass
+
+class Gate
+	# 運賃と駅名を配列として用意
+	STATIONS = [:umeda, :juso, :mikuni]
+	FARES = [150, 190]
+	# initializeで引数として駅名を受け取れるようにする
+	def initialize(name)
+		@name = name
+	end
+
+	# enterメソッドとexitメソッドを実装
+	# enterメソッドは引数として渡されたticket（切符）に自分の駅名を保存
+	def enter(ticket)
+		# Ticketクラスのstampメソッドを呼び出す
+		ticket.stamp(@name)
+	end
+
+
+
+	def exit(ticket)
+		# 適切な運賃と切符の購入額を比較
+		fare = calc_fare(ticket)
+		fare <= ticket.fare
+	end
+
+	def calc_fare(ticket)
+		from = STATIONS.index(ticket.stamped_at)
+		# indexは配列の中から引数に合致する要素の添え字（（:juso）なら１）を取得
+		to = STATIONS.index(@name)
+		# 降車駅の添え字　－　乗車駅の添え字で区間の長さを取得
+		distance = to - from
+		# 「区間の長さ　－　１」を添え字として適切な運賃を取得
+		FARES[distance - 1]
+	end
+end
---
+++ test/gate_test.rb
@@ -0,0 +1,39 @@
+# 改札機プログラムのテストコード
+
+require 'minitest/autorun'
+require './lib/gate'
+require './lib/ticket'
+
+class GateTest < Minitest::Test
+	# テストメソッド実行の前に毎回これが呼ばれる
+	def setup
+		@umeda = Gate.new(:umeda)
+		@juso = Gate.new(:juso)
+		@mikuni = Gate.new(:mikuni)
+	end
+
+	# 最初のテストシナリオ　150円の切符を購入　梅田から十三　結果：　出場できる
+	def test_umeda_to_juso
+		ticket = Ticket.new(150)
+		@umeda.enter(ticket)
+		assert @juso.exit(ticket)
+	end
+	# 2つ目のシナリオ　150円の切符を購入　梅田から三国　結果：　運賃不足
+	def test_umeda_to_mikuni_when_fare_is_not_enough
+		ticket = Ticket.new(150)
+		@umeda.enter(ticket)
+		refute @mikuni.exit(ticket)
+	end
+	# 3つ目のシナリオ　190円　梅田in 三国out　出場できる
+	def test_umeda_to_mikuni_when_fare_is_enough
+		ticket = Ticket.new(190)
+		@umeda.enter(ticket)
+		assert @mikuni.exit(ticket)
+	end
+	# 4つ目　　　　　150円　十三in 三国out　出場できる
+	def test_juso_to_mikuni
+		ticket = Ticket.new(150)
+		@juso.enter(ticket)
+		assert @mikuni.exit(ticket)
+	end
+end
