6ce9912c7b403a33e5489ef976c133b0c1d1ceab
Kamitotomi <rockyaaokage@gmail.com>
2020-10-10 18:18:07 +0900

    muri

--- lib/Student.rb
+++ lib/Student.rb
@@ -32,3 +32,4 @@ class Score
 
 
 end
+
--- lib/deviation.rb
+++ lib/deviation.rb
@@ -5,4 +5,22 @@
 
 class Student
 
+	attr_reader :first_name, :score
+
+	def initialize(first_name, score)
+		@first_name = first_name
+		@score = score
+	end
+
+	# initializeは外部から参照できないのでとりあえずscoreだけ
+	def individual_score
+		"#{score}"
+	end
+	
+
 end
+
+students = []
+students << Student.new('Haruka', 300)
+students << Student.new('Akane', 300)
+
